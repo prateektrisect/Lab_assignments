edcc75a4b4e19cc54c237300c83730734cd9b4c3
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-02 14:48:27 +0530

    Update main.java

--- P/T1/main.java
+++ P/T1/main.java
@@ -1,2 +1,11 @@
-// HI my main file
+public class SumAB
+{
+  public static void main(String[] args)
+  {
+    int a = 10;
+    int b = 20;
+    int sum = a + b;
+    System.out.println(sum);
+  }
+}  
 
