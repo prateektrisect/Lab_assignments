3db5541bdae74dbe7a5702c47bd731bdccccf362
prateektrisect <108329551+prateektrisect@users.noreply.github.com>
2023-01-03 21:05:08 +0530

    Update Main.java

--- P/T4/Main.java
+++ P/T4/Main.java
@@ -1,10 +1,23 @@
-import java.io.*;
-
 public class Main{
-  Scanner scanner = new Scanner(System.in);
-  public static void main(String[] args)
-  {   int n = scanner.nextInt();
-      // write code here
+
+  void solution(int n) {
+
+    // Here testing system will provide
+    // values to variable n, you don't 
+    // need to assign any values to them
     
+    // Write your code here
+    
+
+    System.out.println();  // calculate and print cube
+  }
+
+  //Test code
+  //Don't change anything below this line
+  public static void main(String[] args){
+    Main test = new Main();
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    test.solution(n);
   }
 }
