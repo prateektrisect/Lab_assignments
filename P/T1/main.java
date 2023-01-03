e7e14b7d228d64c28a5704e66190bef794ee4c83
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-03 15:00:01 +0530

    Update main.java

--- P/T1/main.java
+++ P/T1/main.java
@@ -1,11 +1,8 @@
-public class SumAB
+public class Main
 {
   public static void main(String[] args)
   {
-    int a = 10;
-    int b = 20;
-    int sum = a + b;
-    System.out.println(sum);
+//     WRITE CODE HERE
   }
 }  
 
