330810a14f461ad12e8656dbfe691d81be68f75d
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-03 17:36:29 +0530

    Update Main.java

--- P2/T3/Main.java
+++ P2/T3/Main.java
@@ -1,1 +1,11 @@
+import java.io.*;
+
+public class Main{
+  Scanner scanner = new Scanner(System.in);
+  public static void main(String[] args)
+  {   int n = scanner.nextInt();
+      // write code here
+    
+  }
+}
 
