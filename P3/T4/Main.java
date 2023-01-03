5fe2d3e5cef6600d9ef94df76e33988aaef8c912
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-03 17:57:48 +0530

    Create Main.java

---
+++ P3/T4/Main.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main{
+  Scanner scanner = new Scanner(System.in);
+  public static void main(String[] args)
+  {   int n = scanner.nextInt();
+      // write code here
+    
+  }
+}
