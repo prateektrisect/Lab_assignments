d069b5087645fec5bf5e7b467ee1fc5496f9ec0b
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-03 17:16:20 +0530

    Update Main.java

--- P/T2/Main.java
+++ P/T2/Main.java
@@ -1,6 +1,9 @@
+import java.io.*;
+
 public class Main{
+  Scanner scanner = new Scanner(System.in);
   public static void main(String[] args)
-  {
+  {   int n = scanner.nextInt();
       // write code here
     
   }
