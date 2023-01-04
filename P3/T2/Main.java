bbddc5da1aca5193d3a3280d5ecf96f67691b1e1
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-04 13:18:07 +0530

    Update Main.java

--- P3/T2/Main.java
+++ P3/T2/Main.java
@@ -1,10 +1,22 @@
 import java.io.*;
 
 public class Main{
-  Scanner scanner = new Scanner(System.in);
-  public static void main(String[] args)
-  {   int n = scanner.nextInt();
-      // write code here
-    
+
+  void solution() {
+    // Write your code here
+    // Remember the output should match exactly
+    // Use for loop
+    // Starting from 1 till n print 'Pepsi' if the number is divisible by 3 
+    // else coke if the number is not divisible by 3.
+
+
+  }
+
+  //Test code
+  //Don't change anything below this line
+  public static void main(String[] args){
+    Main test = new Main();
+    Scanner sc = new Scanner(System.in);
+    test.solution();
   }
 }
