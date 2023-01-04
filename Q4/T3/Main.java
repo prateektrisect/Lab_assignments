f0bb4b19c2ebd3691c4366210b8a4fb381e03cda
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-04 15:52:35 +0530

    Update Main.java

--- Q4/T3/Main.java
+++ Q4/T3/Main.java
@@ -1,1 +1,23 @@
+import java.io.*;
+
+public class Main{
+
+  void solution() {
+    // Write your code here
+    // Remember the output should match exactly
+    // Use For loop to take each element of string
+    // Use if else to get desired elsment 
+    // if we get element b/w 'A' to 'B' then add it to new string 
+
+
+  }
+
+  //Test code
+  //Don't change anything below this line
+  public static void main(String[] args){
+    Main test = new Main();
+    Scanner sc = new Scanner(System.in);
+    test.solution();
+  }
+}
 
