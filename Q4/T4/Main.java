18cb2f899b4a0d748ce30078c7c445054c906f4b
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-04 15:59:37 +0530

    Update Main.java

--- Q4/T4/Main.java
+++ Q4/T4/Main.java
@@ -1,1 +1,21 @@
 
+public class Main{
+
+  void solution() {
+    // Write your code here
+    // Remember the output should match exactly
+    // Use For loop to take each element of string
+    // Use if else to get desired elsment 
+
+
+  }
+
+  //Test code
+  //Don't change anything below this line
+  public static void main(String[] args){
+    Main test = new Main();
+    Scanner sc = new Scanner(System.in);
+    test.solution();
+  }
+}
+
