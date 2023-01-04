7816cd881965cb01cf0ad7eeabf2c906bcefeae4
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-04 12:32:27 +0530

    Update Main.java

--- P2/T1/Main.java
+++ P2/T1/Main.java
@@ -1,8 +1,18 @@
-public class Main
-{
-  public static void main(String[] args)
-  {
-    // write code here
+public class Main{
+
+  void solution() {
+    // Write your code here
+    // Remember the output should match exactly
+    // Print 1 to 8 on screen using a For loop
+
+
+  }
+
+  //Test code
+  //Don't change anything below this line
+  public static void main(String[] args){
+    Main test = new Main();
+    Scanner sc = new Scanner(System.in);
+    test.solution();
   }
 }
-
