e8737ea123c837a487cc19e962be7ed9fcc98534
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-04 14:39:28 +0530

    Update main.java

--- P4/T1/main.java
+++ P4/T1/main.java
@@ -1,9 +1,20 @@
-public class Task1
-{
-  public static void main(String[] args)
-  {
-    // write code here
+public class Main{
+
+  void solution() {
+    // Write your code here
+    // Remember the output should match exactly
+    // Use if else
+    // Print following:
+    // 'Hello' if n is divisible by 5
+    // else 'Java' otherwise
+
+  }
+
+  //Test code
+  //Don't change anything below this line
+  public static void main(String[] args){
+    Main test = new Main();
+    Scanner sc = new Scanner(System.in);
+    test.solution();
   }
 }
-
-
