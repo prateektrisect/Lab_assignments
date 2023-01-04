f5aa1e9be31c66820c7c3b182a6453b8366c7795
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-04 15:45:23 +0530

    Update Main.java

--- Q4/T1/Main.java
+++ Q4/T1/Main.java
@@ -1,1 +1,20 @@
+import java.io.*;
 
+public class Main{
+
+  void solution() {
+    // Write your code here
+    // Remember the output should match exactly
+    // Use For loop to take each element of string 
+    //  every occurrence of character 'd' is doubled.
+
+  }
+
+  //Test code
+  //Don't change anything below this line
+  public static void main(String[] args){
+    Main test = new Main();
+    Scanner sc = new Scanner(System.in);
+    test.solution();
+  }
+}
