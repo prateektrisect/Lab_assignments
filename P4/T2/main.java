90c4b5ce12863f82ce1cd0b22ae1ea0ac3b7e457
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-04 14:42:40 +0530

    Update main.java

--- P4/T2/main.java
+++ P4/T2/main.java
@@ -1,9 +1,21 @@
-public class Task1
-{
-  public static void main(String[] args)
-  {
-    // write code here
+
+public class Main{
+
+  void solution() {
+    // Write your code here
+    // Remember the output should match exactly
+    // Use if else with For loop
+    // For every number from 1 to n
+    // print 'X' if the number is even
+    // print 'Y' otherwise
+
+  }
+
+  //Test code
+  //Don't change anything below this line
+  public static void main(String[] args){
+    Main test = new Main();
+    Scanner sc = new Scanner(System.in);
+    test.solution();
   }
 }
-
-
