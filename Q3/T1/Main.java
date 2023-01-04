704da68a2df78f931e3c45e6c8bfea27b5b2b8af
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-04 15:38:08 +0530

    Update Main.java

--- Q3/T1/Main.java
+++ Q3/T1/Main.java
@@ -1,1 +1,21 @@
+import java.io.*;
 
+public class Main{
+
+  void solution() {
+    // Write your code here
+    // Remember the output should match exactly
+    // Use For loop to take each element of string 
+    // use If else to select only needed elsments
+
+
+  }
+
+  //Test code
+  //Don't change anything below this line
+  public static void main(String[] args){
+    Main test = new Main();
+    Scanner sc = new Scanner(System.in);
+    test.solution();
+  }
+}
