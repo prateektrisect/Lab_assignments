d0a9f08bf28137b19fc8562aa24840a2b6b1ff6d
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-04 13:10:16 +0530

    Update Main.java

--- P3/T1/Main.java
+++ P3/T1/Main.java
@@ -1,10 +1,19 @@
-import java.io.*;
-
 public class Main{
-  Scanner scanner = new Scanner(System.in);
-  public static void main(String[] args)
-  {   int n = scanner.nextInt();
-      // write code here
-    
+
+  void solution() {
+    // Write your code here
+    // Remember the output should match exactly
+    // Use if else 
+    // Print 'Trisect' if n is odd. Print 'Institute' otherwise
+
+
+  }
+
+  //Test code
+  //Don't change anything below this line
+  public static void main(String[] args){
+    Main test = new Main();
+    Scanner sc = new Scanner(System.in);
+    test.solution();
   }
 }
