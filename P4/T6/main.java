6e86209372c49d3c9c14251016ea0b9e1c11b0f9
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-04 15:19:21 +0530

    Update main.java

--- P4/T6/main.java
+++ P4/T6/main.java
@@ -1,9 +1,22 @@
-public class Task1
-{
-  public static void main(String[] args)
-  {
-    // write code here
+import java.io.*;
+
+public class Main{
+
+  void solution() {
+    // Write your code here
+    // Remember the output should match exactly
+    // Use for loop
+    // Print all the powers of x from 1 to y i.e. x1 x2 x3 x4... xn
+
+
+  }
+
+  //Test code
+  //Don't change anything below this line
+  public static void main(String[] args){
+    Main test = new Main();
+    Scanner sc = new Scanner(System.in);
+    Scanner ss = new Scanner(System.in);
+    test.solution();
   }
 }
-
-
