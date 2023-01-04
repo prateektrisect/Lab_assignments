936c6f41e4f4dc6d37e9dc9c50d0379dd56be9bc
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-04 15:28:24 +0530

    Update Main.java

--- Q1/T2/Main.java
+++ Q1/T2/Main.java
@@ -1,1 +1,21 @@
+import java.io.*;
 
+public class Main{
+
+  void solution() {
+    // Write your code here
+    // Remember the output should match exactly
+    // Use loop with if else 
+    // Print total number of uppercase and lowercase characters. 
+
+
+  }
+
+  //Test code
+  //Don't change anything below this line
+  public static void main(String[] args){
+    Main test = new Main();
+    Scanner sc = new Scanner(System.in);
+    test.solution();
+  }
+}
