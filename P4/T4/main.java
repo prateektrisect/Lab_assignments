2c19ca99ad8c590ff43b6766b5b51247763b7deb
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-04 14:51:32 +0530

    Update main.java

--- P4/T4/main.java
+++ P4/T4/main.java
@@ -1,9 +1,21 @@
-public class Task1
-{
-  public static void main(String[] args)
-  {
-    // write code here
+import java.io.*;
+
+public class Main{
+
+  void solution() {
+    // Write your code here
+    // Remember the output should match exactly
+    // Use for loop
+    // Run the loop Backword and use accumulator to achive pattern
+
+
+  }
+
+  //Test code
+  //Don't change anything below this line
+  public static void main(String[] args){
+    Main test = new Main();
+    Scanner sc = new Scanner(System.in);
+    test.solution();
   }
 }
-
-
