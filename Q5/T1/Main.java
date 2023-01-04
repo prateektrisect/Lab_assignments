95e7961c90b9cec8f8e6db1a750a039adbab9256
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-04 17:02:29 +0530

    Update Main.java

--- Q5/T1/Main.java
+++ Q5/T1/Main.java
@@ -1,1 +1,28 @@
+import java.util.Scanner;
+public class App{
 
+  void solution(int[] array) {
+      
+      int len=array.length;
+
+   
+    for(int i = 0; i < len; i++) {            
+        System.out.println( );  // print the desire output.
+    }
+  }
+
+  //Test code
+  //Don't change anything below this line
+  public static void main(String[] args){
+    App test = new App();
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] array = new int[n]; 
+    for(int i=0; i<n; i++)  
+    {  
+         
+        array[i]=sc.nextInt();  
+    }  
+    test.solution(array);
+  }
+}
