b9491ce52215788bfcb791280f731813bb63cf7c
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-04 12:37:43 +0530

    Update Main.java

--- P2/T2/Main.java
+++ P2/T2/Main.java
@@ -1,10 +1,22 @@
 import java.io.*;
 
 public class Main{
-  Scanner scanner = new Scanner(System.in);
-  public static void main(String[] args)
-  {   int n = scanner.nextInt();
-      // write code here
-    
+
+  void solution() {
+    // Write your code here
+    // Remember the output should match exactly
+    // Write print statement outside of loop ("Start")
+    // Print 1 to 8 on screen using a For loop
+    // Write print statement outside of loop ("End")
+
+
+  }
+
+  //Test code
+  //Don't change anything below this line
+  public static void main(String[] args){
+    Main test = new Main();
+    Scanner sc = new Scanner(System.in);
+    test.solution();
   }
 }
