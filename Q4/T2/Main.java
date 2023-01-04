7ab9deaecd041832278936dcf65a09df5953eabd
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-04 15:50:11 +0530

    Create Main.java

--- Q4/T2/Main.java
+++ Q4/T2/Main.java
@@ -1,1 +1,22 @@
+import java.io.*;
 
+public class Main{
+
+  void solution() {
+    // Write your code here
+    // Remember the output should match exactly
+    // Use For loop to take each element of string
+    // Use if else to get desired elsment 
+    // if we get 'A' then double it and else if we get 'a' the triple it
+
+
+  }
+
+  //Test code
+  //Don't change anything below this line
+  public static void main(String[] args){
+    Main test = new Main();
+    Scanner sc = new Scanner(System.in);
+    test.solution();
+  }
+}
