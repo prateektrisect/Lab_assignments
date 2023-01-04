2ca1d35c65a3dfc88a0b7dcda1471ace4356ff45
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-04 13:30:34 +0530

    Update Main.java

--- P3/T4/Main.java
+++ P3/T4/Main.java
@@ -1,10 +1,20 @@
 import java.io.*;
 
 public class Main{
-  Scanner scanner = new Scanner(System.in);
-  public static void main(String[] args)
-  {   int n = scanner.nextInt();
-      // write code here
-    
+
+  void solution() {
+    // Write your code here
+    // Remember the output should match exactly
+    // Use Two For loop
+    // 1st for pattern and Second for num
+
+  }
+
+  //Test code
+  //Don't change anything below this line
+  public static void main(String[] args){
+    Main test = new Main();
+    Scanner sc = new Scanner(System.in);
+    test.solution();
   }
 }
