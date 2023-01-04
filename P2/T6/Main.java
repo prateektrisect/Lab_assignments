4fc9e952c94af4a5879babdc022edd5b1129b3c7
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-04 13:05:05 +0530

    Update Main.java

--- P2/T6/Main.java
+++ P2/T6/Main.java
@@ -1,10 +1,26 @@
-import java.io.*;
-
 public class Main{
-  Scanner scanner = new Scanner(System.in);
-  public static void main(String[] args)
-  {   int n = scanner.nextInt();
-      // write code here
-    
+
+  void solution() {
+
+    // Here testing system will provide
+    // values to variable n, you don't 
+    // need to assign any values to them
+    // Complete the sample code provided below
+    String pattern="#";
+    for(int i = 0; i <= n; i++) { 
+      
+      // Use accumulator 
+      
+        System.out.println();  //print pattern here in format given
+    }
+  }
+
+  //Test code
+  //Don't change anything below this line
+  public static void main(String[] args){
+    Main test = new Main();
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    test.solution();
   }
 }
