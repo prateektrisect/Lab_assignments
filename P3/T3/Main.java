e4cffad4585dbefc0216f6bd6aff8310e4bc1998
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-04 13:26:36 +0530

    Update Main.java

--- P3/T3/Main.java
+++ P3/T3/Main.java
@@ -1,10 +1,21 @@
 import java.io.*;
 
 public class Main{
-  Scanner scanner = new Scanner(System.in);
-  public static void main(String[] args)
-  {   int n = scanner.nextInt();
-      // write code here
-    
+
+  void solution() {
+    // Write your code here
+    // Remember the output should match exactly
+    // Reffrence P2 T6
+    // Use For loop 
+
+
+  }
+
+  //Test code
+  //Don't change anything below this line
+  public static void main(String[] args){
+    Main test = new Main();
+    Scanner sc = new Scanner(System.in);
+    test.solution();
   }
 }
