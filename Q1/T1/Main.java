94521eaf0c6676ddaad37232fb5074ef3d5fb41e
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-04 15:24:25 +0530

    Update Main.java

--- Q1/T1/Main.java
+++ Q1/T1/Main.java
@@ -1,1 +1,21 @@
+import java.io.*;
 
+public class Main{
+
+  void solution() {
+    // Write your code here
+    // Remember the output should match exactly
+    // Use if else with For loop
+    // Check if character is z then count +1
+
+
+  }
+
+  //Test code
+  //Don't change anything below this line
+  public static void main(String[] args){
+    Main test = new Main();
+    Scanner sc = new Scanner(System.in);
+    test.solution();
+  }
+}
