6d797a22e241170742ef0c236fec02c5ef9f4c28
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-04 13:06:45 +0530

    Update Main.java

--- P2/T4/Main.java
+++ P2/T4/Main.java
@@ -6,7 +6,8 @@ public class Main{
     // Write your code here
     // Remember the output should match exactly
     // Use For loop to get 1 to n
-    // Use Ex(sum=sum*i)
+    // Use accumulator inside loop given below
+    // Use Example(sum=sum*i)
 
 
   }
