a7d535d6ac7df81585855d9ac4f96d655e653cd0
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-04 14:48:31 +0530

    Update main.java

--- P4/T3/main.java
+++ P4/T3/main.java
@@ -1,9 +1,22 @@
-public class Task1
-{
-  public static void main(String[] args)
-  {
-    // write code here
+public class Main{
+
+  void solution() {
+    // Write your code here
+    // Remember the output should match exactly
+    // Use for loop 
+    // Reference P2 T4
+    // Print the factorial of n.
+    // Note: Factorial of n = 1*2*3*4*.....*n [total product from 1 to n]
+
+
+
+  }
+
+  //Test code
+  //Don't change anything below this line
+  public static void main(String[] args){
+    Main test = new Main();
+    Scanner sc = new Scanner(System.in);
+    test.solution();
   }
 }
-
-
