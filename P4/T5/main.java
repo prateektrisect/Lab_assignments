54ef6f7abfba4053ae9dabf9d9584c420822373f
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-04 15:03:34 +0530

    Update main.java

--- P4/T5/main.java
+++ P4/T5/main.java
@@ -1,9 +1,22 @@
-public class Task1
-{
-  public static void main(String[] args)
-  {
-    // write code here
+import java.io.*;
+
+public class Main{
+
+  void solution() {
+    // Write your code here
+    // Remember the output should match exactly
+    // Use 2 for loop to achive pattern  
+    // 1st for ascending order and 2nd for descending order
+
+
+  }
+
+  //Test code
+  //Don't change anything below this line
+  public static void main(String[] args){
+    Main test = new Main();
+    Scanner sc = new Scanner(System.in);
+    test.solution();
   }
 }
-
 
