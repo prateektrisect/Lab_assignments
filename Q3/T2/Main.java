afb5a1f6675e360d8ebe1ca712db0e982ed3eb60
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-04 15:41:40 +0530

    Update Main.java

--- Q3/T2/Main.java
+++ Q3/T2/Main.java
@@ -1,1 +1,21 @@
+import java.io.*;
 
+public class Main{
+
+  void solution() {
+    // Write your code here
+    // Remember the output should match exactly
+    // Use For loop to take each element of string 
+    // use If else to select only needed elsments and replce it  every 1 to "ab" and 9 to "c" 
+
+
+  }
+
+  //Test code
+  //Don't change anything below this line
+  public static void main(String[] args){
+    Main test = new Main();
+    Scanner sc = new Scanner(System.in);
+    test.solution();
+  }
+}
