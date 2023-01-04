dda3f714f417450dbe0d8944b898d3fdbd131387
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-04 12:45:40 +0530

    Update Main.java

--- P2/T4/Main.java
+++ P2/T4/Main.java
@@ -1,10 +1,21 @@
 import java.io.*;
 
 public class Main{
-  Scanner scanner = new Scanner(System.in);
-  public static void main(String[] args)
-  {   int n = scanner.nextInt();
-      // write code here
-    
+
+  void solution() {
+    // Write your code here
+    // Remember the output should match exactly
+    // Use For loop to get 1 to n
+    // Use Ex(sum=sum*i)
+
+
+  }
+
+  //Test code
+  //Don't change anything below this line
+  public static void main(String[] args){
+    Main test = new Main();
+    Scanner sc = new Scanner(System.in);
+    test.solution();
   }
 }
