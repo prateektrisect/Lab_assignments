5a8f138a41dd2e56775e083b7977f3a31b8b9cb9
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-05 16:15:32 +0530

    Update Main.java

--- R1/T2/Main.java
+++ R1/T2/Main.java
@@ -1,1 +1,29 @@
+import java.util.Scanner;
+public class App{
 
+  void solution(int[] array) {
+
+
+        // print the desire output. 
+        // Use For loop with if else to get desire number.
+        // print length of new array
+        
+
+    }
+  }
+
+  //Test code
+  //Don't change anything below this line
+  public static void main(String[] args){
+    App test = new App();
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] array = new int[n]; 
+    for(int i=0; i<n; i++)  
+    {  
+         
+        array[i]=sc.nextInt();  
+    }  
+    test.solution(array);
+  }
+}
