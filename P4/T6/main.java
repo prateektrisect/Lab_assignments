05c7dd1b5e66ffddbbb38b8039d63d753b94466f
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-07 12:44:14 +0530

    Update main.java

--- P4/T6/main.java
+++ P4/T6/main.java
@@ -1,12 +1,11 @@
-import java.io.*;
+import java.util.Scanner;
 
 public class Main{
 
-  void solution() {
+  void solution(int n1, int n2) {
     // Write your code here
     // Remember the output should match exactly
-    // Use for loop
-    // Print all the powers of x from 1 to y i.e. x1 x2 x3 x4... xn
+
 
 
   }
@@ -17,6 +16,8 @@ public class Main{
     Main test = new Main();
     Scanner sc = new Scanner(System.in);
     Scanner ss = new Scanner(System.in);
-    test.solution();
+    int a = sc.nextInt();
+    int b = ss.nextInt();
+    test.solution(a,b);
   }
 }
