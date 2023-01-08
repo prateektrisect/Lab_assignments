6077f79a4978284f743c0a010e8413ba94ed4747
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-08 11:33:29 +0530

    Update Main.java

--- P4/T2/Main.java
+++ P4/T2/Main.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
+public class Main{
+
+  void solution(int n) {
+    // Remember the output should match exactly
+    // Use if else
+    // Write your code here
+  }
+
+  //Test code
+  //Don't change anything below this line
+  public static void main(String[] args){
+    Main test = new Main();
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    test.solution(n);
+  }
+}
 
