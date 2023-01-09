70335be3b639009f66d2731b8858f4094b4f411a
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-10 00:05:58 +0530

    Create Main.java

---
+++ S10/T3/Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.util.Scanner;
+public class Main{
+
+  void solution(String[] array) {
+      
+    // Write code here
+   
+  }
+
+  //Test code
+  //Don't change anything below this line
+  public static void main(String[] args){
+    Main test = new Main();
+    Scanner sc = new Scanner(System.in);
+    String str = sc.nextLine();
+    String[] array = new String[str]; 
+    for(int i=0; i<n; i++)  
+    {  
+         
+        array[i]=sc.nextLine();  
+    }  
+    test.solution(array);
+  }
+}
