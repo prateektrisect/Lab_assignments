ec356f51a0e32cbb3efeb311c2ff10479f441896
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-09 16:27:48 +0530

    Update Main.java

--- Q5/T1/Main.java
+++ Q5/T1/Main.java
@@ -1,19 +1,22 @@
 import java.util.Scanner;
-public class App{
+public class Main{
 
   void solution(int[] array) {
       
-      int len=array.length;
-      int[] array = new int[len]; 
+     int len = array.length;
+     int[] newarray = new int[len];
 
-   
     for(int i = 0; i < len; i++) { 
-      
+      // Write code here
+      // access element from given array
+      // each element is multiplied by 10
       // Assign value to the new Array
-    }
-    for(int j=0;j<len;j++) {
       
-      System.out.println( );  // each element is multiplied by 10
+      
+    }
+    for(int j = 0; j < len; j++) {
+      
+      System.out.println(); // print elements of new array 
     }
   }
 
