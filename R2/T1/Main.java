3194b349829187484a7269a93df1c672083f28f0
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-09 21:24:22 +0530

    Update Main.java

--- R2/T1/Main.java
+++ R2/T1/Main.java
@@ -1,1 +1,27 @@
+import java.util.*;
+import java.util.Scanner;
+public class Main{
 
+  void solution(int[] array) {
+      
+    // Write code here
+   
+    
+   
+  }
+
+  //Test code
+  //Don't change anything below this line
+  public static void main(String[] args){
+    Main test = new Main();
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] array = new int[n]; 
+    for(int i=0; i<n; i++)  
+    {  
+         
+        array[i]=sc.nextInt();  
+    }  
+    test.solution(array);
+  }
+}
