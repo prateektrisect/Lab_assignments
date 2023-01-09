08589d106f5e1d6e16151aed528ed682ce7bbb57
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-09 22:53:56 +0530

    Update Main.java

--- R4/T2/Main.java
+++ R4/T2/Main.java
@@ -1,1 +1,22 @@
 
+import java.util.*;
+import java.util.Scanner;
+
+public class Main{
+
+  void solution(String str) {
+    // Write your code here
+    // Remember the output should match exactly
+  
+  }
+
+  //Test code
+  //Don't change anything below this line
+  public static void main(String[] args){
+    Main test = new Main();
+    Scanner sc = new Scanner(System.in);
+    String str = sc.nextLine();
+    test.solution(n);
+  }
+}
+
