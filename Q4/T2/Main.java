a4d253c8f9cc2677b78598a19820f4585b4b0312
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-09 13:13:25 +0530

    Update Main.java

--- Q4/T2/Main.java
+++ Q4/T2/Main.java
@@ -5,9 +5,7 @@ public class Main{
   void solution() {
     // Write your code here
     // Remember the output should match exactly
-    // Use For loop to take each element of string
-    // Use if else to get desired elsment 
-    // if we get 'A' then double it and else if we get 'a' the triple it
+   
 
 
   }
