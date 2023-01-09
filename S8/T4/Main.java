20b9472e2b318cdebb2e123c0333081b5e949d6a
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-09 23:58:02 +0530

    Create Main.java

---
+++ S8/T4/Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class Main{
+
+  void solution(String str, String rtr) {
+    // Write your code here
+    // Remember the output should match exactly
+  
+  }
+
+  //Test code
+  //Don't change anything below this line
+  public static void main(String[] args){
+    Main test = new Main();
+    
+    Scanner sc = new Scanner(System.in);
+    String str = sc.nextLine();
+    
+     Scanner ss = new Scanner(System.in);
+    String rtr = ss.nextLine();
+    
+    test.solution(str,rtr);
+  }
+}
