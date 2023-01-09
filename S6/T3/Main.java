89515266369aba77dccffd96356e5f9225393757
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-09 23:38:40 +0530

    Create Main.java

---
+++ S6/T3/Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class Main{
+
+  void solution(String str, int n) {
+    // Write your code here
+    // Remember the output should match exactly
+  
+  }
+
+  //Test code
+  //Don't change anything below this line
+  public static void main(String[] args){
+    Main test = new Main();
+    
+    Scanner sc = new Scanner(System.in);
+    String str = sc.nextLine();
+    
+     Scanner ss = new Scanner(System.in);
+    int n = ss.nextInt();
+    
+    test.solution(str,n);
+  }
+}
