5cca6e77dfb9175ba042d829743fb659831bf983
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-09 13:14:48 +0530

    Update Main.java

--- Q2/T2/Main.java
+++ Q2/T2/Main.java
@@ -1,1 +1,33 @@
+import java.util.Scanner;
 
+public class Main{
+
+  void solution(String str1) {
+    // Write your code here
+    // Remember the output should match exactly
+   
+  
+    For(int i=0;i<str1.length();i++)
+  {
+    Char ch=str1.charAt(i);
+    if(ch>='a' && ch<='z')
+    {
+      System.out.println(ch+":"+"small")
+    }
+    
+   if( ch>='A' && ch<='Z')
+    {
+      System.out.println(ch+":"+"capital")
+    }
+     
+  }
+
+  //Test code
+  //Don't change anything below this line
+  public static void main(String[] args){
+    Main test = new Main();
+    Scanner sc = new Scanner(System.in);
+    String st = sc.next();
+    test.solution(st);
+  }
+}
