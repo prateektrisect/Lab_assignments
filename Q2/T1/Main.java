127b1e33e8a26fe16357c46e9c18c7e871bd26df
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-09 13:22:59 +0530

    Update Main.java

--- Q2/T1/Main.java
+++ Q2/T1/Main.java
@@ -3,23 +3,10 @@ import java.util.Scanner;
 
 public class Main{
 
-  void solution(String str1) {
+  void solution() {
     // Write your code here
     // Remember the output should match exactly
-    // Use if else with For loop
-    // Check if character is z then count +1
-    Int count=0;
-    for(int i=0;i<str1.length;i++)
-    {
-      char ch=str1.charAt(i);
-      if(ch=='x' || ch=='y')
-      {
-        int count=count+1;
-      System.out.println(ch+"#"+count);
-      System.ount.println(ch+"#"+count);
-      }
       
-
   }
 
   //Test code
@@ -27,7 +14,6 @@ public class Main{
   public static void main(String[] args){
     Main test = new Main();
     Scanner sc = new Scanner(System.in);
-    String st = sc.next();
-    test.solution(st);
+    test.solution();
   }
 }
