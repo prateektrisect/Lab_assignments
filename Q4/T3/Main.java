9e6e5c6dd164aa09d41b536591ab1a3f23efc625
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-09 13:19:01 +0530

    Update Main.java

--- Q4/T3/Main.java
+++ Q4/T3/Main.java
@@ -1,14 +1,11 @@
-import java.io.*;
+import java.util.Scanner;
 
 public class Main{
 
   void solution() {
     // Write your code here
     // Remember the output should match exactly
-    // Use For loop to take each element of string
-    // Use if else to get desired elsment 
-    // if we get element b/w 'A' to 'B' then add it to new string 
-
+    
 
   }
 
