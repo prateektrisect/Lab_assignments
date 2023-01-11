0a68f2fd9763b12349114e75c22e23c6bc396953
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-11 16:36:52 +0530

    Update Main.java

--- P1/T3/Main.java
+++ P1/T3/Main.java
@@ -2,15 +2,9 @@ import java.util.Scanner;
 public class Main{
 
   void solution(int n) {
-
-    // Here testing system will provide
-    // values to variable n, you don't 
-    // need to assign any values to them
-    
-    // Write your code here
     // Complete the sample code provided below
     for(int i = 1; i <= n; i++) {            
-        System.out.println();  //print i here
+      // write your code here
     }
   }
 
