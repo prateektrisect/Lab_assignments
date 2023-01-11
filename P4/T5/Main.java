2219f6b552109f9a94eb5d39ced3982debbcc37f
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-11 17:08:08 +0530

    Update Main.java

--- P4/T5/Main.java
+++ P4/T5/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main{
 
   void solution(int n) {
+  
     // Write your code here
     // Remember the output should match exactly
     // Use 2 for loop 
@@ -16,8 +17,8 @@ public class Main{
   public static void main(String[] args){
     Main test = new Main();
     Scanner sc = new Scanner(System.in);
-    int num = sc.nextInt();
-    test.solution(num);
+    int n = sc.nextInt();
+    test.solution(n);
   }
 }
 
