9d9c12e7c2978da71711ab4df585174cce8d3b8b
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-11 13:48:21 +0530

    Update Main.java

--- S10/T1/Main.java
+++ S10/T1/Main.java
@@ -1,1 +1,31 @@
 
+import java.util.*;
+import java.util.Scanner;
+public class Main{
+
+  void solution(ArrayList<Integer> arrli) {
+      
+    // Write code here
+    // Create new array list named it as narlist
+    
+    
+    System.out.println(); //print arraylist using toString() like: narlist.toString()
+  }
+
+  //Test code
+  //Don't change anything below this line
+  public static void main(String[] args){
+    Main test = new Main();
+    ArrayList<Integer> arrli = new ArrayList<>();
+    Scanner sc = new Scanner(System.in);
+    
+    int n = sc.nextInt();
+    int[] array = new int[n]; 
+    
+    for(int i=0; i<n; i++)  
+    {  arrli.add(sc.nextInt());  
+    }  
+    test.solution(arrli);
+  }
+}
+
