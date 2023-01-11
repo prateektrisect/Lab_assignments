dea57c544cc431e33f5e815f04a03fa1cfe1c7e6
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-11 17:36:41 +0530

    Update Main.java

--- P2/T4/Main.java
+++ P2/T4/Main.java
@@ -5,13 +5,6 @@ public class Main{
   void solution(int x) {
     // Remember the output should match exactly
     // Write your code here
-   
-    // Use For loop to get 1 to n
-    // Use accumulator pattern inside the loop 
-    // print the result outside for loop
-    
-
-
   }
 
   //Test code
