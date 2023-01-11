d74d68e18188bc3dd7a00300dda1e28956a9e86b
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-11 17:25:26 +0530

    Update Main.java

--- P4/T6/Main.java
+++ P4/T6/Main.java
@@ -16,8 +16,8 @@ public class Main{
     Main test = new Main();
     Scanner sc = new Scanner(System.in);
     Scanner ss = new Scanner(System.in);
-    int a = sc.nextInt();
-    int b = ss.nextInt();
-    test.solution(a,b);
+    int n1 = sc.nextInt();
+    int n2 = ss.nextInt();
+    test.solution(n1,n2);
   }
 }
