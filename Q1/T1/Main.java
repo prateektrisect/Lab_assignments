7e9d2e54f7b332957d9ab8db517927ce99b9149f
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-12 13:48:55 +0530

    Update Main.java

--- Q1/T1/Main.java
+++ Q1/T1/Main.java
@@ -1,14 +1,18 @@
 import java.util.Scanner;
-
 public class Main{
-
-  void solution(String str1) {
+  void solution(String str) {
+    
+   int len = str.length();
+   
+    for(int i=0 ;i<len; i++)
+    {
+      
     // Write your code here
-    // Remember the output should match exactly
-    // Use if else with For loop
+    // Remember the output should match exactly  
+    // Use if else with For loop 
     // Check if character is z then count +1
-
-
+      
+    }
   }
 
   //Test code
@@ -16,7 +20,7 @@ public class Main{
   public static void main(String[] args){
     Main test = new Main();
     Scanner sc = new Scanner(System.in);
-    String st = sc.nextInt();
-    test.solution(st);
+    String str = sc.nextLine();
+    test.solution(str);
   }
 }
