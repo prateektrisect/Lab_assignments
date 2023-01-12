dab950d33ed31da5ccb9d4a6d8b6c83edf51cf3d
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-12 15:30:02 +0530

    Update Main.java

--- R6/T3/Main.java
+++ R6/T3/Main.java
@@ -2,18 +2,18 @@ import java.util.*;
 import java.util.Scanner;
 public class Main{
 
-  void solution(int[] array, int [] aaray) {
+  void solution(int[] array , int[] array1) {
       
     // Write code here
+    // print new array here with this statment
+    // System.out.println(Arrays.toString()) // type name of new array inside Arrays.toString()
    
-    
   }
 
   //Test code
   //Don't change anything below this line
   public static void main(String[] args){
     Main test = new Main();
-    
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
     int[] array = new int[n]; 
@@ -21,17 +21,15 @@ public class Main{
     {  
          
         array[i]=sc.nextInt();  
-    } 
-    
-     Scanner sc = new Scanner(System.in);
-    int x = sc.nextInt();
+    }  
+     Scanner ss = new Scanner(System.in);
+    int x = ss.nextInt();
     int[] array1 = new int[x]; 
     for(int i=0; i<x; i++)  
     {  
          
         array1[i]=sc.nextInt();  
-    } 
+    }  
     test.solution(array,array1);
   }
 }
-
