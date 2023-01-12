35ee7fd6c5116bbbc28144ffb474bffce6693654
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-12 14:45:41 +0530

    Update Main.java

--- R1/T1/Main.java
+++ R1/T1/Main.java
@@ -1,30 +1,22 @@
-
+import java.util.*;
 import java.util.Scanner;
-public class App{
+public class Main{
 
-  void solution(int[] array) {
-
-
-        // print the desire output. 
-        // Use For loop with if else to get desire number.
-     
-        
-
-    }
+  void solution(int n) {
+  
+      
+    // Write code here
+    // print new array here
+    
+   
   }
 
   //Test code
   //Don't change anything below this line
   public static void main(String[] args){
-    App test = new App();
+    Main test = new Main();
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
-    int[] array = new int[n]; 
-    for(int i=0; i<n; i++)  
-    {  
-         
-        array[i]=sc.nextInt();  
-    }  
-    test.solution(array);
+    test.solution(int n);
   }
 }
