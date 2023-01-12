fd28bec7bf8eb54f95b0692095332125c3e340f1
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-12 14:24:44 +0530

    Update Main.java

--- Q5/T2/Main.java
+++ Q5/T2/Main.java
@@ -1,20 +1,27 @@
 import java.util.Scanner;
-public class App{
+public class Main{
 
   void solution(int[] array) {
       
-      int len=array.length;
-    
+     int len = array.length;
+     int[] newarray = new int[len];
+
     for(int i = 0; i < len; i++) { 
+    
+    
+      // Write code here
+      // access element from given array
+      // Print the number which is nearest to 1000.
       
-      //  Find gratest number
     }
+    System.out.println(Arrays.toString(); // type name of new array inside Arrays.toString()
+   
   }
 
   //Test code
   //Don't change anything below this line
   public static void main(String[] args){
-    App test = new App();
+    Main test = new Main();
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
     int[] array = new int[n]; 
