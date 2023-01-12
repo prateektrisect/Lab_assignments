fbdbc5011429643e86ae7f091ed7d5b47c8732ec
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-12 15:06:26 +0530

    Update Main.java

--- R2/T4/Main.java
+++ R2/T4/Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.util.Scanner;
 public class Main{
 
-  void solution(int[] array) {
+  void solution(String str) {
       
     // Write code here
   
@@ -13,7 +13,7 @@ public class Main{
   public static void main(String[] args){
     Main test = new Main();
     Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-    test.solution(n);
+    String str = sc.nextLine();
+    test.solution(str);
   }
 }
