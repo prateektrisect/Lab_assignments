e1d02aa594f595dbb67c35bde140c3e03fcb52c0
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-12 13:57:40 +0530

    Update Main.java

--- Q2/T2/Main.java
+++ Q2/T2/Main.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
-
 public class Main{
-
-  void solution() {
+  void solution(String str) {
+    
+     
     // Write your code here
-    // Remember the output should match exactly
+    // Remember the output should match exactly  
+    
    
-  
   }
 
   //Test code
@@ -14,6 +14,7 @@ public class Main{
   public static void main(String[] args){
     Main test = new Main();
     Scanner sc = new Scanner(System.in);
-     test.solution();
+    String str = sc.nextLine();
+    test.solution(str);
   }
 }
