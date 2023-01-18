5569ab6f80a420016e3cee709903035aab77d828
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-17 23:47:59 -0800

    Update Main.java

--- S2/T4/Main.java
+++ S2/T4/Main.java
@@ -1,21 +1,23 @@
 import java.util.*;
 import java.util.Scanner;
 
+
 public class Main{
 
-  void solution(String str) {
-    // Write your code here
-    // Remember the output should match exactly
-  
-  }
-
   //Test code
-  //Don't change anything below this line
   public static void main(String[] args){
     Main test = new Main();
     Scanner sc = new Scanner(System.in);
     String str = sc.nextLine();
-    test.solution(n);
+    
+//  create an object of Main class here
+// call your function  
   }
+  
+  
+//   Create your function here
+  
+  
+  
 }
 
