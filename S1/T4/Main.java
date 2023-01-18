f4f59c448c3042a73eaf91de5aa9803f71de6b3b
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-17 23:34:07 -0800

    Create Main.java

---
+++ S1/T4/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.Scanner;
+
+
+public class Main{
+	public static void main(String[] args)
+ 	{
+ 	  Main test=new Main();
+  
+ 	}
+
+ 	public void square(int n)
+ 	{
+ 	
+    }	
+ }
+
