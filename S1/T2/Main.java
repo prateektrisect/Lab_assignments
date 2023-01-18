78ddbb7e7a615181b57a05936e7d4569058429c2
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-17 21:48:52 -0800

    Create Main.java

---
+++ S1/T2/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+  
+  void square(int n) {
+	//write code here:
+	
+	}
+
+  public static void main(String[] args) {		
+	Main test=new Main();
+    	Scanner sc=new Scanner(System.in);
+    	int x=sc.NextInt();
+    	test.square(x);
+
+	}
+
+}
