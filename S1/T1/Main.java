0b5b3bd32478673617f4f65a7e0f8a03747ba437
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-17 21:37:07 -0800

    Update Main.java

--- S1/T1/Main.java
+++ S1/T1/Main.java
@@ -1,1 +1,17 @@
+public class Main {
+  
+	public void square(int n) {
+		int squaren=n*n;
+		System.out.println(squaren);
+	}
+
+  public static void main(String[] args) {		
+		S1T1 obj=new S1T1();
+    Scanner sc=new Scanner(System.in);
+    int x=sc.NextInt();
+		obj.square(x);
+
+	}
+
+}
 
