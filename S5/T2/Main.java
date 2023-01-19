d53ca287e594f260d6965205cd62e138af4dade5
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-18 22:55:54 -0800

    Update Main.java

--- S5/T2/Main.java
+++ S5/T2/Main.java
@@ -1,29 +1,24 @@
 import java.util.*;
 import java.util.Scanner;
+
+
 public class Main{
-
-  void solution(String[] array) {
-      
-    // Write code here
-   
-    
-//     print new array here
-        System.out.println(Arrays.toString()) // type name of new array inside Arrays.toString()
-   
-  }
-
-  //Test code
-  //Don't change anything below this line
+  
   public static void main(String[] args){
+  //create the object of the main class
     Main test = new Main();
     Scanner sc = new Scanner(System.in);
-    Strind str = sc.nextLine();
-    String[] array = new String[str]; 
-    for(int i=0; i<n; i++)  
-    {  
-         
-        array[i]=sc.nextLine();  
-    }  
-    test.solution(array);
+    int n = sc.nextInt();
+    Scanner sc1 = new Scanner(System.in);
+    String[] array1=new String[n];
+    for(int i=0;i<n;i++)
+    {
+	    array1[i]=sc1.nextLine();
+    }
+   //Call the Function with object.
+  //test.function_name()  using format.
   }
+
+  //create the function and write code here.
 }
+
