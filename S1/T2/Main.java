5bf651ee00e17ce4842c419701424da45de25889
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-19 16:30:46 +0530

    Update Main.java

--- S1/T2/Main.java
+++ S1/T2/Main.java
@@ -1,18 +1,19 @@
 import java.util.*;
-import java.util.Scanner;
-
 
 public class Main{
   
   public static void main(String[] args){
-  //create the object of the main class
-    Main test = new Main();
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-   //Call the Function with object.
-  //test.function_name()  using format.
+  
+    Main test = new Main();   //created object of the main class
+  
+   //Write code here
+   //Call the Function with object
+   //test.function_name()  using format
+  
+  
   }
-
-  //create the function and write code here.
+ 
+  //copy square() function here
+  
 }
 
