b39493a0a16ae457f7db413f9aad49f3341595fc
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-19 22:42:29 -0800

    Update Main.java

--- S6/T4/Main.java
+++ S6/T4/Main.java
@@ -1,36 +1,32 @@
 import java.util.*;
 import java.util.Scanner;
+
+
 public class Main{
-
-  void solution(int[] array, int [] aaray) {
-      
-    // Write code here
-   
-    
-  }
-
-  //Test code
-  //Don't change anything below this line
+  
   public static void main(String[] args){
+  //create the object of the main class
     Main test = new Main();
-    
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
-    int[] array = new int[n]; 
-    for(int i=0; i<n; i++)  
-    {  
-         
-        array[i]=sc.nextInt();  
-    } 
-    
-     Scanner sc = new Scanner(System.in);
-    int x = sc.nextInt();
-    int[] array1 = new int[x]; 
-    for(int i=0; i<x; i++)  
-    {  
-         
-        array1[i]=sc.nextInt();  
-    } 
-    test.solution(array,array1);
+    Scanner sc1 = new Scanner(System.in);
+    int [] array1=new int[n];
+    for(int i=0;i<n;i++)
+    {
+	    array1[i]=sc1.nextInt();
+    }
+    Scanner sc2 = new Scanner(System.in);
+    int n1 = sc2.nextInt();
+    Scanner sc3 = new Scanner(System.in);
+    int [] array2=new int[n];
+    for(int i=0;i<n1;i++)
+    {
+	    array1[i]=sc3.nextInt();
+    }
+   //Call the Function with object.
+  //test.function_name()  using format.
   }
+
+  //create the function and write code here.
 }
+
