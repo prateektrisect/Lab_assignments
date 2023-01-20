1c154eaa7661d2267017fc1ff385d1993b22f464
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-19 22:58:34 -0800

    Update Main.java

--- S8/T4/Main.java
+++ S8/T4/Main.java
@@ -1,25 +1,21 @@
 import java.util.*;
 import java.util.Scanner;
 
+
 public class Main{
-
-  void solution(String str, String rtr) {
-    // Write your code here
-    // Remember the output should match exactly
   
+  public static void main(String[] args){
+  //create the object of the main class
+    Main test = new Main();
+    Scanner sc = new Scanner(System.in);
+    String s = sc.nextLine();
+    Scanner sc1 = new Scanner(System.in);
+    String s2 = sc.nextLine();
+
+   //Call the Function with object.
+  //test.function_name()  using format.
   }
 
-  //Test code
-  //Don't change anything below this line
-  public static void main(String[] args){
-    Main test = new Main();
-    
-    Scanner sc = new Scanner(System.in);
-    String str = sc.nextLine();
-    
-     Scanner ss = new Scanner(System.in);
-    String rtr = ss.nextLine();
-    
-    test.solution(str,rtr);
-  }
+  //create the function and write code here.
 }
+
