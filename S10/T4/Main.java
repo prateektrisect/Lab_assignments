cb5d6f27accb1a72426d7ffb350a6d913d5f4e2a
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-19 23:09:29 -0800

    Update Main.java

--- S10/T4/Main.java
+++ S10/T4/Main.java
@@ -1,26 +1,24 @@
 import java.util.*;
 import java.util.Scanner;
+
+
 public class Main{
-
-  void solution(int[] array) {
-      
-    // Write code here
-   
-    
-  }
-
-  //Test code
-  //Don't change anything below this line
+  
   public static void main(String[] args){
+  //create the object of the main class
     Main test = new Main();
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
-    int[] array = new int[n]; 
-    for(int i=0; i<n; i++)  
-    {  
-         
-        array[i]=sc.nextInt();  
-    }  
-    test.solution(array);
+    Scanner sc1 = new Scanner(System.in);
+    int [] array1=new int[n];
+    for(int i=0;i<n;i++)
+    {
+	    array1[i]=sc1.nextInt();
+    }
+   //Call the Function with object.
+  //test.function_name()  using format.
   }
+
+  //create the function and write code here.
 }
+
