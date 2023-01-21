bdf5de225156d55da8934e8ab7f4c556659c5b2f
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-21 10:11:28 +0530

    Update Main.java

--- S4/T1/Main.java
+++ S4/T1/Main.java
@@ -5,12 +5,14 @@ import java.util.Scanner;
 public class Main{
   
   public static void main(String[] args){
-  //create the object of the main class
+  //created an object of the main class
     Main test = new Main();
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
+    
    //Call the Function with object.
   //test.function_name()  using format.
+  
   }
 
   //create the function and write code here.
