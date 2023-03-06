96294211acf88b0eebe4a64148173f433b3ea22e
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-03-06 15:34:15 +0530

    Update Main.java

--- S3/T4/Main.java
+++ S3/T4/Main.java
@@ -3,20 +3,8 @@ import java.util.Scanner;
 
 
 public class Main{
-  
-  public static void main(String[] args){
-    Main test = new Main();	//created an object of the main class
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-    Scanner sc1 = new Scanner(System.in);
-    int [] array1=new int[n];
-    for(int i=0;i<n;i++)
-    {
-	    array1[i]=sc1.nextInt();
-    }
-   //Call the Function with object.
-  
-  }
+	
+	
 # Given function
 void drinks(ele){
   if((ele%2==0) && (ele%3!=0)){
@@ -32,10 +20,25 @@ void drinks(ele){
      System.out.println("Pepsi");
      }
  }
+	
+	
  // Write code here
   //create the function here.
   
+//   Test code
+ public static void main(String[] args){
+    Main test = new Main();	//created an object of the main class
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    Scanner sc1 = new Scanner(System.in);
+    int [] array1=new int[n];
+    for(int i=0;i<n;i++)
+    {
+	    array1[i]=sc1.nextInt();
+    }
+   //Call the Function with object.
   
+  }
   
 }
 
