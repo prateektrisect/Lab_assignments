068faca06c36c73debb3994b7d3539ba52ba92fc
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-03-06 15:31:49 +0530

    Update Main.java

--- S3/T2/Main.java
+++ S3/T2/Main.java
@@ -4,6 +4,21 @@ import java.util.Scanner;
 
 public class Main{
   
+	
+	
+// Given function
+void evenOdd(int ele){
+  if(n%2==0){
+    System.out.println(ele + ":even");
+  }
+  else{    
+	  System.out.println(ele + ":odd");
+  }
+	
+  //write code here
+  //create the function.
+	
+// Test code
   public static void main(String[] args){
     Main test = new Main();		//created an object of the main class
     Scanner sc = new Scanner(System.in);
@@ -17,18 +32,5 @@ public class Main{
 
   }
 
-// Given function
-void evenOdd(int ele){
-  if(n%2==0){
-    System.out.println(ele + ":even");
-  }
-  else{    
-	  System.out.println(ele + ":odd");
-  }
-	
-  //write code here
-  //create the function.
-	
-	
 }
 
