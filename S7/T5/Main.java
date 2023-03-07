d2cd1283682e44019619e0752bd6ca9da807f8fe
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-03-07 12:45:57 +0530

    Update Main.java

--- S7/T5/Main.java
+++ S7/T5/Main.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 
 public class Main{
   
+
+  //create the function and write code here.
+	
+// test code	
   public static void main(String[] args){
   //create the object of the main class
     Main test = new Main();
@@ -18,7 +22,5 @@ public class Main{
    //Call the Function with object.
   //test.function_name()  using format.
   }
-
-  //create the function and write code here.
 }
 
