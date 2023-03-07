cd4a714eb4263fcdbc82204788ea3030775b851c
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-03-07 12:42:52 +0530

    Update Main.java

--- S6/T4/Main.java
+++ S6/T4/Main.java
@@ -4,29 +4,30 @@ import java.util.Scanner;
 
 public class Main{
   
+	
+	
+  //create the function and write code here.
+	
+//   test code	
   public static void main(String[] args){
   //create the object of the main class
     Main test = new Main();
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
-    Scanner sc1 = new Scanner(System.in);
     int [] array1=new int[n];
     for(int i=0;i<n;i++)
     {
-	    array1[i]=sc1.nextInt();
+	    array1[i]=sc.nextInt();
     }
-    Scanner sc2 = new Scanner(System.in);
-    int n1 = sc2.nextInt();
-    Scanner sc3 = new Scanner(System.in);
+   
+    int n1 = sc.nextInt();
     int [] array2=new int[n];
     for(int i=0;i<n1;i++)
     {
-	    array1[i]=sc3.nextInt();
+	    array1[i]=sc.nextInt();
     }
    //Call the Function with object.
   //test.function_name()  using format.
   }
-
-  //create the function and write code here.
 }
 
