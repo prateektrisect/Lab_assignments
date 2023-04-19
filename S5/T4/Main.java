9638c816e4d4360798cecdfb91a373cd54e6f5d9
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-04-19 10:13:58 +0530

    Update Main.java

--- S5/T4/Main.java
+++ S5/T4/Main.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.util.Scanner;
 
 
 public class Main{
@@ -22,7 +21,7 @@ public class Main{
     }
     int n1 = sc.nextInt();
     int [] array2=new int[n1];
-    for(int i=0;i<n;i++)
+    for(int i=0;i<n1;i++)
     {array2[i]=sc.nextInt();
     }
     
